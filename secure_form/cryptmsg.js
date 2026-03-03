3662a5e9c19a6be86ce2ce06b86005003cda073e
D <embellish4648@proton.me>
2026-03-02 20:17:54 -0600

    Fixed comment.

--- secure_form/cryptmsg.js
+++ secure_form/cryptmsg.js
@@ -37,8 +37,8 @@ document.addEventListener('DOMContentLoaded', function() {
         // This is intended to be a kind of ersatz TOTP implementation. It
         // probably doesn't do much but is mostly here to deter bots.
         /*
-        const key_resp = await fetch('https://rf.mt.oits.fail/pubweb_form_key?\
-                                      pk=MTI3MTQK');
+        const key_resp = await fetch('https://rf.mt.oits.fail/' + '
+                                     'pubweb_form_key?pk=MTI3MTQK');
         const key = await key_resp.json();
         if (key) {
             form.querySelector('#k').value = key;
