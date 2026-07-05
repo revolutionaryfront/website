e91f756a2903c16e945babc91a1668697b59152d
D <embellish4648@proton.me>
2026-07-05 18:23:04 -0500

    Updates

--- _next/static/LlfRj6bEvmUSKzPI2Bna0/_buildManifest.js
+++ _next/static/LlfRj6bEvmUSKzPI2Bna0/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/website//_next/:path+",
-        "destination": "/website/_next/:path+"
+        "source": "/${repoName}//_next/:path+",
+        "destination": "/${repoName}/_next/:path+"
       }
     ],
     "fallback": []
