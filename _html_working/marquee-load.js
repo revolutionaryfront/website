0f503b1c8d59aa3e62706b65e6c7fa534af5d247
D <embellish4648@proton.me>
2026-07-15 13:11:21 -0500

    Started tracking html/css working files. I probably should have put this in a separate branch, but I don't know if there is any way to do that without removing and re-adding everything so, this is just going to stay here.

---
+++ _html_working/marquee-load.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+    fetch('/rfwebsite/_html_working/test-alert-data.json', {
+        method: 'GET',
+        credentials: 'include',
+        mode: 'no-cors',
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+            //document.getElementById('marquee').style.display = 'none';
+            document.querySelector('#marquee').style.visibility = 'hidden';
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data) {
+            if (data[0]) {
+                document.querySelector('#marquee').style.visibility = 'visible';
+                document.getElementById('marquee-text-left').innerHTML = '<a href="' + data[0][0] + '">' + data[0][1] + '</a>';
+                document.getElementById('marquee-text-center').innerHTML = '<a href="' + data[0][0] + '">' + data[0][1] + '</a>';
+                document.getElementById('marquee-text-right').innerHTML = '<a href="' + data[0][0] + '">' + data[0][1] + '</a>';
+            }
+            if (data[1]) {
+                document.getElementById('marquee-text-center').innerHTML = '<a href="' + data[1][0] + '">' + data[1][1] + '</a>';
+            }
+            if (data[2]) {
+                document.getElementById('marquee-text-right').innerHTML = '<a href="' + data[2][0] + '">' + data[2][1] + '</a>';
+            }
+        } else {
+            document.querySelector('#marquee').style.visibility = 'hidden';
+        }
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        document.querySelector('#marquee').style.visibility = 'hidden';
+    });
+}
