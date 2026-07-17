e57ca5102924971cf4cb2a600bd3dfa3f606d32d
D <embellish4648@proton.me>
2026-07-17 05:14:58 -0500

    Added points-of-unity.html and styles for it.

--- _html_working/marquee-load.js
+++ _html_working/marquee-load.js
@@ -7,8 +7,8 @@ window.onload = function() {
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
-            //document.getElementById('marquee').style.display = 'none';
-            document.querySelector('#marquee').style.visibility = 'hidden';
+            document.getElementById('marquee').style.display = 'none';
+            //document.querySelector('#marquee').style.visibility = 'hidden';
         }
         return response.json();
     })
@@ -16,6 +16,7 @@ window.onload = function() {
         if (data) {
             if (data[0]) {
                 document.querySelector('#marquee').style.visibility = 'visible';
+                document.getElementById('marquee').style.display = 'block';
                 document.getElementById('marquee-text-left').innerHTML = '<a href="' + data[0][0] + '">' + data[0][1] + '</a>';
                 document.getElementById('marquee-text-center').innerHTML = '<a href="' + data[0][0] + '">' + data[0][1] + '</a>';
                 document.getElementById('marquee-text-right').innerHTML = '<a href="' + data[0][0] + '">' + data[0][1] + '</a>';
@@ -27,11 +28,13 @@ window.onload = function() {
                 document.getElementById('marquee-text-right').innerHTML = '<a href="' + data[2][0] + '">' + data[2][1] + '</a>';
             }
         } else {
-            document.querySelector('#marquee').style.visibility = 'hidden';
+            //document.querySelector('#marquee').style.visibility = 'hidden';
+            document.getElementById('marquee').style.display = 'none';
         }
     })
     .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
-        document.querySelector('#marquee').style.visibility = 'hidden';
+        //document.querySelector('#marquee').style.visibility = 'hidden';
+        document.getElementById('marquee').style.display = 'none';
     });
 }
